e07e767e4e92750567034f72f71661834a4b12df
이예민 <lier@kakao.com>
2022-02-09 20:36:15 +0900

    feat: NavBar 틀 구현

---
+++ components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="m-4 mt-4 mx-6 flex justify-between">
+    <div class="text-3xl inline-flex">
+      <div class="font-extralight">
+        CORK
+      </div>
+      <div>
+        LOG
+      </div>
+    </div>
+    <div>
+      <button class="px-2.5 py-1.5 rounded-md hover:bg-gray-200 focus:ring focus:ring-gray-100">
+        회원가입
+      </button>
+      <button class="ml-2 px-4 py-1.5 rounded-md text-white shadow-md bg-blue-500 hover:bg-blue-400 focus:ring focus:ring-blue-300">
+        로그인
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial />
+  <div class="container mx-auto lg:px-20">
+    <NavBar />
+  </div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    NavBar
+  }
 }
 </script>
