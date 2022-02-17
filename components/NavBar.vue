7da3b2b8ea3fa343264b286fb094741f333f5d5d
이예민 <lier@kakao.com>
2022-02-17 18:33:04 +0900

    fix: nuxt-link시 레이아웃 수정

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="m-4 mt-4 mx-6 flex justify-between">
-    <div class="text-3xl inline-flex">
+    <nuxt-link to="/" class="text-3xl inline-flex">
       <div class="font-extralight">
         CORK
       </div>
       <div>
         LOG
       </div>
-    </div>
+    </nuxt-link>
     <div v-if="auth == true">
       <nuxt-link to="/signup">
         회원가입
       </nuxt-link>
-      <nuxt-link to="/login" class="ml-3 px-4 py-2 rounded-md text-white shadow-md bg-blue-500 hover:bg-blue-400 focus:ring focus:ring-blue-300">
-        로그인
+      <nuxt-link to="/login">
+        <button class="ml-3 px-4 py-2 rounded-md text-white shadow-md bg-blue-500 hover:bg-blue-400 focus:ring focus:ring-blue-300">
+          로그인
+        </button>
       </nuxt-link>
     </div>
   </div>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,14 +3,14 @@
     <form>
       <div class="px-8 py-6 w-screen max-w-sm">
         <div class="space-y-5">
-          <div class="text-4xl inline-flex">
+          <nuxt-link to="/" class="text-4xl inline-flex">
             <div class="font-extralight">
               CORK
             </div>
             <div>
               LOG
             </div>
-          </div>
+          </nuxt-link>
           <div>
             <label for="email" class="block mb-1 text-gray-600 font-semibold">이메일</label>
             <input type="text" class="bg-blue-50 px-4 py-2 outline-none rounded-md w-full">
